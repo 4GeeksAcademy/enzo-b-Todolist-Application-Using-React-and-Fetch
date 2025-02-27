09d3b7764606677c43d229bbc62cfebd2dad25bf
enzobarrera <154301070+enzobarrera@users.noreply.github.com>
2025-02-27 17:53:38 +0000

    obtener tareas y crear usuario

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [todos, setTodos] = useState([]);
+	const [newTodo, setNewTodo] = useState("");
+
+	const obtenerTareas = async ()=> {
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/enzobarrera")
+			console.log(response.status)
+			if (response.status==404){
+				await crearUsuario()
+				return
+			}
+			const data = await response.json()
+			console.log(data.todos)
+			setTodos(data.todos)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const crearUsuario = async()=>{
+		try {
+			const response = await fetch("https://playground.4geeks.com/todo/users/enzobarrera", {
+				method: "POST", 
+				headers: {"Content-Type":"application/json"}
+			})
+			console.log(response)
+			if(response.status == 201){
+				await obtenerTareas()
+				return
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	const addTodo = (e) => {
+		if (e.key === "Enter" && newTodo.trim() !== "") {
+			setTodos([...todos, newTodo]);
+			setNewTodo("");
+		}
+	};
+
+	const removeTodo = (index) => {
+		setTodos(todos.filter((_, i) => i !== index));
+	};
+
+	useEffect(()=>{
+		obtenerTareas()
+	}, [])
+	return (
+		<div className="todo-container">
+			<h1 className="title">todos</h1>
+			<div className="input-container">
+				<input
+					type="text"
+					className="todo-input"
+					placeholder="What needs to be done?"
+					value={newTodo}
+					onChange={(e) => setNewTodo(e.target.value)}
+					onKeyDown={addTodo}
+				/>
+			</div>
+
+			<ul className="todo-list">
+				{todos.map((todo, index) => (
+					<li key={index} className="todo-item">
+						{todo.label}
+						<button className="delete-button" onClick={() => removeTodo(index)}>✖</button>
+					</li>
+				))}
+			</ul>
+			<p className="todo-count">{todos.length} item(s) left</p>
+
+
 		</div>
 	);
 };
